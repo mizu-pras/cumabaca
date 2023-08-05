62cc5e43c70db160e146f91dd8fb97c69a911bdd
Mi and Zu <dfebri446@gmail.com>
2023-08-06 03:09:08 +0700

    implement memory cache

--- routes/komik.js
+++ routes/komik.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const cache = require('memory-cache');
 const { fetchData, validateUrl } = require('../utils/index')
+
+const ONE_DAY = 3600 * 24;
+const cacheMiddleware = (duration) => {
+    return (req, res, next) => {
+        let key = '__express__' + req.originalUrl || req.url
+        let cachedBody = cache.get(key)
+        if (cachedBody) {
+            res.send(cachedBody)
+            return
+        } else {
+            res.sendResponse = res.send
+            res.send = (body) => {
+                cache.put(key, body, duration * 1000);
+                res.sendResponse(body)
+            }
+            next()
+        }
+    }
+}
 
 const router = express.Router();
 
-router.get('/chapters', async (req, res, next) => {
+router.get('/chapters', cacheMiddleware(ONE_DAY), async (req, res, next) => {
     const { url } = req.query;
 
     try {
@@ -39,7 +59,7 @@ router.get('/chapters', async (req, res, next) => {
     }
 });
 
-router.get('/data', async (req, res, next) => {
+router.get('/data', cacheMiddleware(ONE_DAY), async (req, res, next) => {
     const { url } = req.query;
 
     try {
